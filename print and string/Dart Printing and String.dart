c6fdd769afc0e72972503db01ebcc7d2f8cdb95c
mdmanik74 <manikmhamud74@outlook.com>
2023-02-26 12:59:07 +0600

    print, string and comments

---
+++ print and string/Dart Printing and String.dart
@@ -0,0 +1,24 @@
+
+void main(){
+  
+  var name= "Md. Manik";
+  var address= "Dhaka, Bangldesh";
+  var road =1205;
+  
+  print("My name $name and location $address $road");
+}
+
+//Types of Comments
+//Dart provides three kinds of comments
+
+//Single-line Comments
+
+// This will print the given statement on screen
+
+
+//Multi-line Comments
+/* This is the example of multi-line comment 
+    This will print the given statement on screen */
+//Documentation Comments
+
+
