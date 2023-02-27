e1d249bd6fd217b6295d7ce6cae0aac34d811bb7
mdmanik74 <manikmhamud74@outlook.com>
2023-02-27 15:00:40 +0600

    constant, codnditional if else for switch

---
+++ conditional/conditional.dart
@@ -0,0 +1,20 @@
+
+void main()
+{
+    int a = 5;
+    int b = 7;
+ 
+    // Conditional Statement
+    var c = (a < 10) ? 
+    print(c);
+ 
+    // Conditional statement
+    int n;
+    var d = n ? ? 
+    print(d);
+ 
+    // After assigning value to n
+    n = 10;
+    d = n ? ? 
+    print(d);
+}
