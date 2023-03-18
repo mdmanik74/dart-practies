b1558c6ec8f0e9c4b617a41ccc3b8bb4112f6b0a
mdmanik74 <manikmhamud74@outlook.com>
2023-03-18 13:50:24 +0600

    keyword and inheritance

---
+++ oop/inheritance.dart
@@ -0,0 +1,17 @@
+class Person {
+  String Fullname(String name){
+    print(name)''
+  }
+  int fullage(int age){
+    print(age);
+  }
+}
+
+class Child extends Person{
+
+}
+main(){
+
+  var childs= new Child();
+  child.Fullname("Manik");
+}
---
+++ oop/keyword.dart
@@ -0,0 +1,18 @@
+class Person{
+  String Name;
+  int age;
+  String address;
+
+  Person (String name, int age, String address){
+    this.Name;
+    print($Name);
+     print($age);
+      print($address);
+  
+
+  }
+}
+
+void main(List<String> args) {
+  Person student = new Person("Md Manik, 25, Dhaka Bangladesh");
+}
