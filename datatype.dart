6d453717885849b1e73013a3f79eda958a15d260
Md. Manik <65363624+mdmanik74@users.noreply.github.com>
2023-09-02 01:23:42 +0600

    Add files via upload
    Dart datatype

---
+++ datatype.dart
@@ -0,0 +1,37 @@
+void main() {
+// print("hello world");
+  
+  int age = 25;
+  print(age);
+  double date=14;
+  print(date);
+  
+  String name = "Md. Manik";
+  print(name);
+  
+  bool value= false;
+  print(value);
+  
+  // List data type
+  
+  List myList =['one','two','three','four'];
+  print(myList);
+  
+  Map myNew={
+'name' : 'Md. Manik',
+    'age': 30,
+    'Address': 'Dhaka',
+  };
+  print(myNew);
+  
+  
+  Set mySet={
+    'One', 'Two', 'Three', 'Four',
+  };
+  print(mySet);
+  
+  
+   final nameTwo ="Md. Manik";
+  print(nameTwo.codeUnits);
+}
+
