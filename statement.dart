2646c99e76d81eb1ec07e18447540d1d01d42dfb
Md. Manik <65363624+mdmanik74@users.noreply.github.com>
2023-09-02 23:36:15 +0600

    Create statement.dart
    string_concatenation, statement

---
+++ statement.dart
@@ -0,0 +1,30 @@
+void main(){
+  String connection ='yConnected';
+  if(connection == 'Connected'){
+  
+  print("Connected");
+  }
+  else{
+    print("NOT Connected");
+  }
+  }
+  
+  Switch(Flutter){
+      case 'Flutter';
+      print('Flutter');
+      break;
+      case 'dart';
+      print('dart');
+      break;
+      defult:
+      print('OK');
+  
+
+  String mList='We' 'Are' 'Learning' 'Dart';
+  print(mList);
+  
+  
+ List <String> f=['We', 'Are','Learning', 'Dart'];
+  
+  print(f.join());
+}
