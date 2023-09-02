85401c3607ff14b0121d8319878b193973304ef7
Md. Manik <65363624+mdmanik74@users.noreply.github.com>
2023-09-02 23:34:35 +0600

    Create operators.dart
    operators

---
+++ operators.dart
@@ -0,0 +1,26 @@
+void main (){
+  int a =10;
+  int b =3;
+  var result = a~/b;
+   print(result);
+  //boolen
+  
+  String name='Md.Manik';
+  var nameR= name is String;
+  print(nameR);
+  
+  
+  //conditional operators
+  
+  String colour ="white";
+  var result1 = colour=='red'?'Color is red':'unknown';
+  print(result1);
+ 
+  
+  int ? age;
+  var ages= age??25;
+  print(ages);
+  
+  
+  
+}
